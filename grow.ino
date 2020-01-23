6f431d0640ebd515b03785b0fb1a6c398aab299d
amarocesar <amaro.txt@gmail.com>
2020-01-22 23:04:06 -0300

    configurando placa nova

--- grow.ino
+++ grow.ino
@@ -1,25 +1,56 @@
 #include <Wire.h>  //INCLUSÃO DE BIBLIOTECA
 #include <LiquidCrystal_I2C.h> //INCLUSÃO DE BIBLIOTECA
- 
-LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE); //FUNÇÃO DO TIPO "LiquidCrystal_I2C"
- 
+#include "DHT.h"
+#include <NTPClient.h>
+#include "ESP8266WiFi.h"
+#include <WiFiUdp.h>
+
+#define DHTPIN 9  // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+//
+//const char *ssid = "Amaro";
+//const char *password = "Marleyyury";
+//
+//WiFiUDP ntpUDP;
+//NTPClient timeClient(ntpUDP);
+
+
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x3F, 16, 2); //FUNÇÃO DO TIPO "LiquidCrystal_I2C"
+
 void setup()
 {
   //inicializa o display (16 colunas x 2 linhas)
-  lcd.begin (16,2); // ou 20,4 se for o display 20x4
+  //lcd.begin (16, 2); // ou 20,4 se for o display 20x4
+  Serial.begin(9600);
+//  lcd.init();
+  dht.begin();
+//  lcd.backlight();
+//  lcd.setCursor(0, 0);
+//  lcd.print("TESTE");
+  
+  //Serial.begin(115200);
+//  WiFi.begin(ssid, password);
+//  while ( WiFi.status() != WL_CONNECTED ) {
+//    delay ( 500 );
+//    Serial.print ( "." );
+//  }
+//  timeClient.begin();
 }
- 
-void loop(){
-    //acende o backlight do LCD
-    lcd.setBacklight(HIGH);
-    //posiciona o cursor para escrita
-    //.setCursor(coluna, linha)
-    lcd.setCursor(0,0);
-    lcd.print("FERNANDOK.COM");
-    lcd.setCursor(0,1);
-    lcd.print("ACESSE!!!");
-    delay(1000); //intervalo de 1s
-    //desliga o backlight do LCD
-    lcd.setBacklight(LOW);
-    delay(1000); //intervalo de 1s
+
+void loop() {
+  //acende o backlight do LCD
+  float humidade = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+//  Serial.println("CONSEGUI GRAVAR");
+   if (isnan(temperatura) || isnan(humidade)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+//    timeClient.update();
+//
+//    Serial.println(timeClient.getFormattedTime());
+  
+    delay(1000);
+//    lcd.print("TESTE");
 }
